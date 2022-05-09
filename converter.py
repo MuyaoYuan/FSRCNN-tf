3de0753b8fa0378db363cbf6a2485caced846fee
yuanmuyuao <yuanmuyao.stu@gmail.com>
2022-05-10 01:03:04 +0800

    转化FSRCNN为tfjs模型，浏览器端测试成功

--- converter.py
+++ converter.py
@@ -4,20 +4,21 @@ import tensorflow as tf
 from tensorflow import keras
 import tensorflowjs as tfjs
 
-from model.SRCNN import bulid_model
+from model.SRCNN import bulid_model as bulid_SRCNN
+from model.FSRCNN import bulid_model as build_FSRCNN
 
 class Converter:
-    def __init__(self):
-        self.trained_model = keras.models.load_model('trained_model/SRCNN.h5', compile=False)
-        self.save_model = bulid_model(n_colors=3)
-        self.save_model.build([1, 240, 320, 3 ])
+    def __init__(self, model_path, tfjs_target_dir):
+        self.trained_model = keras.models.load_model(model_path, compile=False)
+        self.save_model = build_FSRCNN(scale_factor=2, num_channels=3)
+        self.save_model.build([1, 240, 320, 3])
         for layer in self.save_model.layers:
             try:
                 layer.set_weights(self.trained_model.get_layer(name=layer.name).get_weights())
             except:
                 print("Could not transfer weights for layer {}".format(layer.name))
         print(self.save_model.summary())
-        self.tfjs_target_dir = 'tfjs_model'
+        self.tfjs_target_dir = tfjs_target_dir
 
 
     def convert(self):
@@ -25,5 +26,5 @@ class Converter:
 
 
 if __name__  == '__main__':
-    converter = Converter()
+    converter = Converter('trained_model/FSRCNN/FSRCNN.h5', 'tfjs_model/FSRCNN')
     converter.convert()
